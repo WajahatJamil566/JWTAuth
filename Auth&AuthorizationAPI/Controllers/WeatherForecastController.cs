5f02ef20ff360c68fa566b9f9a928c08a19deb3d
WajahatJamil566 <145835388+WajahatJamil566@users.noreply.github.com>
2023-09-24 12:52:18 +0500

    Update WeatherForecastController.cs

--- Auth&AuthorizationAPI/Controllers/WeatherForecastController.cs
+++ Auth&AuthorizationAPI/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace Auth_AuthorizationAPI.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         [HttpGet("Get")]
         public IActionResult Get()
